bd4bc3d0150d43063225cc8ac2debc3bc5ea9c26
Stanislav Bondarenko <sbonda@ttu.ee>
2019-02-09 12:15:48 +0200

    last

--- easyride/js/main.js
+++ easyride/js/main.js
@@ -7,17 +7,18 @@ var submitted = 0;
 var selection = 0;
 var subSelection = 0;
 $('.btnC').on('click',function() {
-    var str = $('.screenNumber').text();
-    $('.screenNumber').text(str.substring(0, str.length - 1));
+    var str = $('.screenNumber').html();
+    $('.screenNumber').html(str.substring(0, str.length - 1));
 });
 
 var timeButtonPressed;
 var lastPressedDigit;
 
 $('.btnNum').on('mousedown', function() {
+    setTimeout(stop,1000);
     if (mode == 1) {
         var num = $(this).data('num');
-        $('.screenNumber').text($('.screenNumber').text() + num);
+        $('.screenNumber').html($('.screenNumber').html() + num);
     } else {
         timeButtonPressed = new Date().valueOf();
         lastPressedDigit = $(this).data('num');
@@ -32,11 +33,11 @@ $('.btnNum').on('mousedown', function() {
         symbols = $(this).data('symbols');
         if (currentNumBtn == numBtn && (currentClickTimestamp - clickTimestamp < 800 )) {
             symbolIndex = numOfClick % symbols.length ? numOfClick % symbols.length - 1 : symbols.length - 1;
-            var str = $('.screenNumber').text();
-            $('.screenNumber').text(str.substring(0, str.length - 1) + symbols[symbolIndex]);
+            var str = $('.screenNumber').html();
+            $('.screenNumber').html(str.substring(0, str.length - 1) + symbols[symbolIndex]);
         } else {
             symbolIndex = 0;
-            $('.screenNumber').text($('.screenNumber').text() + symbols[symbolIndex]);
+            $('.screenNumber').html($('.screenNumber').html() + symbols[symbolIndex]);
         }
         numBtn = currentNumBtn;
         clickTimestamp = currentClickTimestamp;
@@ -44,7 +45,7 @@ $('.btnNum').on('mousedown', function() {
 });
 $('.btnNum').on('mouseup', function(){
     if (mode != 1) {
-        if (new Date().valueOf() - timeButtonPressed > 800){
+        if (new Date().valueOf() - timeButtonPressed > 200){
             var str = $('.screenNumber').text();
             $('.screenNumber').text(str.substring(0, str.length - 1) + lastPressedDigit); 
         } 
@@ -64,28 +65,28 @@ $(".btnNum").mousedown(function(e) {
 });
 
 $('.btnMenu').on('click',function() {
-    // var str = $('.screenNumber').text();
-    // $('.screenNumber').text(str.substring(0, str.length - 1));
+    // var str = $('.screenNumber').html();
+    // $('.screenNumber').html(str.substring(0, str.length - 1));
     if(submitted == 1){
         mode = 0;
-        str = $('.screenNumber').text();
+        str = $('.screenNumber').html();
         selection = str.substring(str.length -1, str.length);
     }
     if(submitted == 4 && selection == 1){
-        str = $('.screenNumber').text();
+        str = $('.screenNumber').html();
         subSelection = str.substring(str.length -1, str.length);
         if($.isNumeric(subSelection)){
             mode = 0;
         }
     }
     if(submitted == 2 && selection == 2){
-        str = $('.screenNumber').text();
+        str = $('.screenNumber').html();
         subSelection = str.substring(str.length -1, str.length);
         if($.isNumeric(subSelection)){
             mode = 0;
         }
     }
-    if(submitted == 0 && $('.screenNumber').text() == "*123#"){
+    if(submitted == 0 && $('.screenNumber').html() == "*123#"){
         mode = 1;
         $('.screenNumber').html("1 - Book a ride <br> 2 - Inquiry/complaint <br> 3 - Add payment method <br>").css( "text-align", "left" );
         submitted++;
@@ -99,60 +100,60 @@ $('.btnMenu').on('click',function() {
         submitted++;
     }
     else if(submitted == 3 && selection == 1){
-        $('.screenNumber').text(" Estimated cost price XX and distance XX and arrive time XX \n Press 1 for accept, 2 for cancel");
+        $('.screenNumber').html(" cost price 3.6€ and estimated distance 2.6km and arrive time 6 minute. <br> Press 1 for accept <br> 2 for cancel <br>");
         submitted++;
         mode = 1;
     }
     else if(submitted == 4 && selection == 1 && subSelection == 1){
-        $('.screenNumber').text("Your ride will arrive soon. You will be informed through the sms.");
+        $('.screenNumber').html("Your ride will arrive soon. You will be informed through the sms.");
         submitted++;
     }
     else if(submitted == 4 && selection == 1 && subSelection == 2){
-        $('.screenNumber').text("Your ride cancelled. Thanks for using our service!");
+        $('.screenNumber').html("Your ride cancelled. Thanks for using our service!");
         submitted++;
     }
     else if(submitted == 5 && selection == 1){
         mode = 1;
-        $('.screenNumber').text("");
+        $('.screenNumber').html("");
         submitted = 0;
     }
     else if(submitted == 1 && selection == 2){
-        $('.screenNumber').text("1- Feedback \n 2- Lost item \n 3- Return to the menu \n\n");
+        $('.screenNumber').html("1- Feedback <br> 2- Lost item <br> 3- Return to the menu<br>");
         submitted++;
         mode = 1;
     }
     else if(submitted == 2 && selection == 2 && subSelection == 1){
-        $('.screenNumber').text("Type your feedback \n");
+        $('.screenNumber').html("Type your feedback <br>");
         submitted++;
     }
     else if(submitted == 3 && selection == 2 && subSelection == 1){
-        $('.screenNumber').text("Thank you your feedback. Details of your feedback, will be send via SMS.\n");
+        $('.screenNumber').html("Thank you your feedback. Details of your feedback, will be send via SMS.<br>");
         submitted++;
     }
     else if(submitted == 4 && selection == 2 && subSelection == 1){
         mode = 1;
-        $('.screenNumber').text("");
+        $('.screenNumber').html("");
         submitted = 0;
     }
     else if(submitted == 2 && selection == 2 && subSelection == 2){
-        $('.screenNumber').text("Name of Item? \n");
+        $('.screenNumber').html("Name of Item? <br>");
         submitted++;
     }
     else if(submitted == 3 && selection == 2 && subSelection == 2){
-        $('.screenNumber').text("Ride reference Number? \n");
+        $('.screenNumber').html("Ride reference Number? <br>");
         submitted++;
     }
     else if(submitted == 4 && selection == 2 && subSelection == 2){
-        $('.screenNumber').text("We are starting to look for your lost item. We will inform you in 24 hour \n");
+        $('.screenNumber').html("We are starting to look for your lost item. We will inform you in 24 hour <br>");
         submitted++;
     }
     else if(submitted == 5 && selection == 2 && subSelection == 2){
         mode = 1;
-        $('.screenNumber').text("");
+        $('.screenNumber').html("");
         submitted = 0;
     }
     else if(submitted == 2 && selection == 2 && subSelection == 3){
-        $('.screenNumber').text("1- Book a ride \n 2- Inquiry / complaint \n 3- Add payment method \n\n");
+        $('.screenNumber').html("1- Book a ride <br> 2- Inquiry / complaint <br> 3- Add payment method <br><br>");
         submitted = 1;
         mode = 1;
     }
@@ -164,15 +165,15 @@ $('.btnPrev').on('click',function(){
     document.getElementById('screen').scrollTop +=10;
 });
 $('.btn').on('click',function() {
-    if($('.screenNumber').text().length < 30){
+    if($('.screenNumber').html().length < 30){
         console.log("heree")
         $(".screenNumber").css({'font-size':24});
     }
     else{
         console.log("heree 2")
-        $(".screenNumber").css({'font-size':14});
+        $(".screenNumber").css({'font-size':12});
     }
-    if ($('.screenNumber').text()) {
+    if ($('.screenNumber').html()) {
         $('.screenNumber').removeClass('hidden');
     } else {
         $('.screenNumber').addClass('hidden');
